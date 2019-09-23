bc29dd8b1ff006989496f3b3171d2cd87972ec6d
HitzeD <d-hitze@comcast.net>
2019-09-23 18:50:55 -0400

    update

--- be/models.py
+++ be/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    fname = models.CharField(max_length=35)
+    lname = models.CharField(max_length=35)
+    title = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(max_length=200)
