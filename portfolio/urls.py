fbd4edaf8152e5f50e0e20a188de9a8121be9596
HitzeD <d-hitze@comcast.net>
2019-09-24 15:13:25 -0400

    created routes

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from backend.api import ContactViewSet, PersonalContactViewSet
+
+# Routes below
+
+router = routers.DefaultRouter()
+router.register(r'contact', ContactViewSet)
+router.register(r'contact/personal', PersonalContactViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
